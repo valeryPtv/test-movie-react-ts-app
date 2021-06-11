d271726925ef8c901f135506a10e8af107ef69d0
valery <valery.ptv@gmail.com>
2021-06-11 14:11:41 +0300

    initialized project config: added eslint, tailwind, removed extra files, modified gitignore

---
+++ postcss.config.ts
@@ -0,0 +1,25 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+    './src/**/*.ts',
+    './src/**/*.tsx',
+    './public/index.html',
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
+const tailwindPlugin = require('tailwindcss');
+const autoprefixerPlugin = require('autoprefixer');
+
+module.exports = {
+  plugins: [
+    tailwindPlugin,
+    autoprefixerPlugin,
+    ...process.env.NODE_ENV === 'production' ? [ purgecss ] : [],
+  ],
+};
